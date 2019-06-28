b875487ebaa69021c330686e06b3f74cb2d5f823
meshkowich <lomonosow@gmail.com>
2019-06-28 14:20:31 +0300

    cool text, great photos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcomeeeee to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcomeeeee to Your Vue.js App"/> -->
+    <stars></stars>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import stars from './components/stars.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    stars
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; 
 }
 </style>
